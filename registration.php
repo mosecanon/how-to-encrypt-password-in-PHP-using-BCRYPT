3bfb807950bde6fee66e7013121532ad830f8f4e
Mose Canon <58547280+mosecanon@users.noreply.github.com>
2024-08-13 11:08:29 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+$connection = new mysqli('localhost', 'root', '', 'youtube');
+
+$name = mysqli_real_escape_string($connection, $_POST['name']);
+$email = mysqli_real_escape_string($connection, $_POST['email']);
+$password = mysqli_real_escape_string($connection, $_POST['password']);
+#encrypt password using BCRYPT / ARGON
+$encrypt = password_hash($password, PASSWORD_BCRYPT);
+
+$insert = $connection -> query("INSERT INTO users(name, email, password) 
+            VALUES('$name', '$email', '$encrypt')");
+if($insert) {
+    echo 'Data inserted successfully.';
+} else {
+    echo 'Failed, Please try again.';
+}
---
+++ signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Password Encryption & Verification | Canon Code </title> 
+    <link rel="stylesheet" href="style.css">
+   </head>
+<body>
+  <div class="wrapper">
+    <h2>Registration</h2>
+    <form action="registration.php" method="POST" enctype="multipart/form-data">
+      <div class="input-box">
+        <input type="text" name="name" placeholder="Enter your name" required>
+      </div>
+      <div class="input-box">
+        <input type="text" name="email" placeholder="Enter your email" required>
+      </div>
+      <div class="input-box">
+        <input type="password" name="password" placeholder="Create password" required>
+      </div>
+      <div class="input-box button">
+        <input type="submit" name="submit_btn" value="Register Now">
+      </div>
+      <div class="text">
+        <h3>Already have an account? <a href="#">Login now</a></h3>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
